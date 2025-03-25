4570950ff1bb6a6816e3a1d75b8ed79292d30522
erickems <erick.sena@ccc.ufcg.edu.br>
2025-03-25 10:31:33 -0300

    Operações com arraylist

--- MyClass.java
+++ MyClass.java
@@ -1,18 +1,20 @@
+import java.util.ArrayList;
 public class MyClass{
 
     public static void main(String args []){
-        int[] mArray = new int[3];
+        ArrayList<Integer> aList = new ArrayList<Integer>();
 
-        System.out.println("Tamanho do array: " + mArray.length);
-        
-        for(int i = 0; i < mArray.length; ++i){
-            mArray[i] = (1 * i) + 2; 
+        for(int i = 0; i < 5; i++){
+            aList.add((i+2) * 3);
         }
 
-        int n = 0;
-        for (int i : mArray) {
-            System.out.println("O elemento na posição " + n + " é: " + i);
-            ++n;
+        System.out.println(aList.toString() + " toString()");
+        System.out.println(aList.get(2) + " get()");
+        System.out.println(aList.indexOf(7) + " indexOf()");
+        System.out.println(aList.size() + " size()");
+
+        for(int i = aList.size() - 1; i >= 0; i--){
+            System.out.println(i);
         }
     }
 }
