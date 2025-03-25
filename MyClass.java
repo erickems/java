67cbe45d5cd37dddd9d12df640ef989c6aec4b04
erickems <erick.sena@ccc.ufcg.edu.br>
2025-03-25 09:28:40 -0300

    Array

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,18 @@
- public class MyClass{
+public class MyClass{
 
     public static void main(String args []){
-        System.out.println("hello world");
+        int[] mArray = new int[3];
+
+        System.out.println("Tamanho do array: " + mArray.length);
+        
+        for(int i = 0; i < mArray.length; ++i){
+            mArray[i] = (1 * i) + 2; 
+        }
+
+        int n = 0;
+        for (int i : mArray) {
+            System.out.println("O elemento na posição " + n + " é: " + i);
+            ++n;
+        }
     }
 }
