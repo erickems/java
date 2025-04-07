92d962f916c9dee06671ba284700e39c3490fbc1
erickems <erick.sena@ccc.ufcg.edu.br>
2025-04-07 13:12:35 -0300

    Adiciona formas de iterar sobre uma fila

--- MyClass.java
+++ MyClass.java
@@ -1,16 +1,12 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class MyClass{
 
     public static void main(String args []) {
-        Queue<Integer> queue  = new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>();
 
         queue.add(3);
         queue.add(2);
-        queue.add(1);
         queue.add(8);
         queue.add(6);
 
@@ -23,5 +19,30 @@ public class MyClass{
         System.out.println(queue.element());
         System.out.println(queue);
 
+        queue.clear();
+
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+
+        System.out.println(queue);
+
+        //Formas de imprimir de modo sequecial
+
+        /*Iterator<Integer> iterator = queue.iterator();
+        while(iterator.hasNext()) {
+            int i = iterator.next();
+            System.out.println(i);
+        }
+
+        for(Integer i : queue) {
+            System.out.println(i);
+        }*/
+
+        queue.stream().forEach((nextElement) -> {
+            System.out.println(nextElement);
+        });
     }
 }
