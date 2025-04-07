1c7da29f6cdf30fe3c5acb291690f3e7e94e1179
erickems <erick.sena@ccc.ufcg.edu.br>
2025-04-07 10:39:22 -0300

    Estudando filas com Queue

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,27 @@
- public class MyClass{
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
-    public static void main(String args []){
-        System.out.println("hello world");
+public class MyClass{
+
+    public static void main(String args []) {
+        Queue<Integer> queue  = new LinkedList<>();
+
+        queue.add(3);
+        queue.add(2);
+        queue.add(1);
+        queue.add(8);
+        queue.add(6);
+
+        System.out.println(queue);
+
+        System.out.println(queue.peek());
+        queue.poll();
+        System.out.println(queue);
+        queue.remove();
+        System.out.println(queue.element());
+        System.out.println(queue);
+
     }
 }
